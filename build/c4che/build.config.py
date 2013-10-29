07c0043219ee6ff99769d638e59c878e9c774c8d
Gianfranco Ceccolini <gianfranco@portalmod.com>
2013-10-29 11:22:59 -0200

    added MOD interfaces for part of the plugins

---
+++ build/c4che/build.config.py
@@ -0,0 +1,2 @@
+version = 0x1070b00
+tools = [{'tool': 'ar', 'tooldir': None, 'funs': None}, {'tool': 'cxx', 'tooldir': None, 'funs': None}, {'tool': 'g++', 'tooldir': None, 'funs': None}, {'tool': 'compiler_cxx', 'tooldir': None, 'funs': None}]
